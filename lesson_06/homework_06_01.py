6c25d800b69c8adefd40f0b2a514d5e1fba9899b
Zelosf <Zelosf@gmail.com>
2025-06-05 22:28:43 +0300

    Виконано всі завдання

---
+++ lesson_06/homework_06_01.py
@@ -0,0 +1,12 @@
+#Порахувати кількість унікальних символів в строці. Якщо їх більше 10 - вивести в консоль True, інакше - False.
+# Строку отримати за допомогою функції input()
+
+
+example_str = input('Введіть строку:')
+unique_elements = set(example_str)
+
+if len(unique_elements) > 10:
+	print('true')
+else:
+	print('false')
+
