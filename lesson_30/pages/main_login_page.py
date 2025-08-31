c2c2568f1ebe60ee821da2e098185affd7663651
Zelosf <Zelosf@gmail.com>
2025-08-31 10:28:02 +0300

    Revert "подготовка"
    This reverts commit fd598e5c82ca4efb3651d5339011d1af8780d858.

--- lesson_30/pages/main_login_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class MainPage:
-	def __init__(self, driver):
-		self.driver = driver
-
-	def sign_in_btn(self):
-		return self.driver.find_element(By.CLASS_NAME("btn btn-outline-white header_signin"))
